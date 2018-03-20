3575b0924c25e6c514e6e1910267d9e50e72e5b1
Osiris <tim@chaubet.be>
2018-03-20 15:50:58 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@
 ## treat env vars
 
 ## hand of to CMD
+
+echo "Copying ProfitTrailer's trading configuration to PTM as base..."
 cp /mnt/profittrailer/trading/* /opt/pt-magic/ptm-binance/_presets/Default/
 
 ping 10.10.0.1
